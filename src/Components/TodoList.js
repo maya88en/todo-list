c370caa30463c128e9be9cac45127b40d194ac07
maya88en <maya88en@gmail.com>
2024-03-13 11:10:45 +0200

    update Todo.js, TodoList.js, task & icons seen when run

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { useState } from 'react'
 import TodoForm from './TodoForm'
+import Todo from './Todo';
 
 export default function TodoList() {
 
 // state Array which hold all TODOS
 const [todos, setTodos] = useState([]);
 
+
+// Add todo to list
 const addTask = task => {
     if (!task.text) {
         return
@@ -19,10 +22,30 @@ const addTask = task => {
 //const oldTask = todos;
 //todos.push(task);
 
+
+// Remove todo from list
+const removeTask = id => {
+  let updatedTasks = [...todos].filter(task => task.id !== id)
+  setTodos(updatedTasks)
+}
+
+// Task is completed
+const completeTask = id => {
+  let updatedTasks = todos.map(todo => {
+    if (todo.id === id) {
+      todo.iscomplete = true;
+    }
+    return todo;
+  })
+  setTodos(updatedTasks)
+}
+
+
   return (
     <div>
-
+        
         <TodoForm addTask={addTask}></TodoForm>
+        <Todo todos={todos} completeTask={completeTask} removeTask={removeTask} ></Todo>
 
     </div>
   )
